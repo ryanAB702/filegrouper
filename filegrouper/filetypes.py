4e5c4fd980f0f11090f346928e176129d20cdb98
Ryan <rab83@duke.edu>
2017-01-31 12:49:05 -0500

    filetypes with added types, file grouper with dictionary instead of list of groups

--- filegrouper/filetypes.py
+++ filegrouper/filetypes.py
@@ -3,15 +3,16 @@ class FileType(object):
         self.typename = typename
         self.comp_func = comp_func
 
-
-def clan_comp_func(input):
-    if input.endswith(".cha"):
-        return True
+def audio_bl_comp_func(input):
+    if input.endswith(".csv"):
+        if "audio" in input:
+            return True
     return False
 
-def bl_comp_func(input):
+def video_bl_comp_func(input):
     if input.endswith(".csv"):
-        return True
+        if "video" in input:
+            return True
     return False
 
 def lena5min_comp_func(input):
@@ -24,11 +25,70 @@ def silences_comp_func(input):
         return True
     return False
 
+def video_comp_func(input):
+    if input.endswith(".mp4"):
+        return True
+    return False
 
-clan_file = FileType("clan", clan_comp_func)
-audio_bl = FileType("audio_bl", bl_comp_func)
+def audio_comp_func(input):
+    if input.endswith(".wav"):
+        if "scrubbed" not in input:
+            return True
+    return False
+
+def audio_scrubbed_comp_func(input):
+    if input.endswith(".wav"):
+        if "scrubbed" in input:
+            return True
+    return False
+
+def opf_not_final_comp_func(input):
+    if input.endswith(".opf"):
+        if "consensus_final" not in input:
+            return True
+    return False
+
+def opf_final_comp_func(input):
+    if input.endswith(".opf"):
+        if "consensus_final" in input:
+            return True
+    return False
+
+def clan_silences_comp_func(input):
+    if input.endswith(".cha"):
+        if "silences" in input:
+            return True
+    return False
+
+def clan_final_comp_func(input):
+    if input.endswith(".cha"):
+        if "_final" in input and "_merged" not in input:
+            return True
+    return False
+
+def clan_merged_comp_func(input):
+    if input.endswith(".cha"):
+        if "newclan_merged" in input and "final" not in input:
+            return True
+    return False
+
+def clan_merged_final_comp_func(input):
+    if input.endswith(".cha"):
+        if "newclan_merged" in input and "final" in input:
+            return True
+    return False
+
+
+opf_not_final = FileType("opf_not_final", opf_not_final_comp_func)
+opf_final = FileType("opf_final",opf_final_comp_func)
+audio_scrubbed = FileType("audio_scrubbed", audio_scrubbed_comp_func)
+audio = FileType("audio", audio_comp_func)
+video_file = FileType("video", video_comp_func)
+audio_bl = FileType("audio_bl", audio_bl_comp_func)
+video_bl = FileType("video_bl", video_bl_comp_func)
 silences = FileType("silences", silences_comp_func)
 lena5min = FileType("lena5min", lena5min_comp_func)
-
-
-
+clan_silences = FileType("clan_silences", clan_silences_comp_func)
+clan_final = FileType("clan_final", clan_final_comp_func)
+newclan_merged = FileType("newclan_merged", clan_merged_comp_func)
+newclan_merged_final = FileType("newclan_merged_final", clan_merged_final_comp_func)
