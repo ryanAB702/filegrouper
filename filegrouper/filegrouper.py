bc85351cc769bd56edc22a036cd5b9240f772b78
Ryan <rab83@duke.edu>
2017-03-31 16:56:26 -0400

    Added docstring comments to classes and functions in filegrouper.py and filetypes.py

--- filegrouper/filegrouper.py
+++ filegrouper/filegrouper.py
@@ -5,6 +5,12 @@ import filetypes
 
 class FileGrouper(object):
     def __init__(self, dir="", prefix_len=5, types=[]):
+        '''
+        :param dir: Directory in which the user is working
+        :param prefix_len: Equals 5 since two digits for subject number, one underscore, and two digits for month
+        :param types: List of the file types being checked for (types found in filetypes.py)
+        '''
+
         self.directory = dir
         self.prefix_len = prefix_len
         self.type_map = {}
@@ -21,8 +27,12 @@ class FileGrouper(object):
 
 
     def walk_directory(self):
-        #curr_group = FileGroup(self.type_map.keys())
+        '''
+        :return: Does not return any value; navigates the files
+        Sets and updates variables to their correct values (pref; curr_group; gurr_group.prefix; groups
+        '''
 
+        #curr_group = FileGroup(self.type_map.keys())
         #while not curr_group.full:
         for root, dirs, files in os.walk(self.directory):
             for file in files:
@@ -40,9 +50,18 @@ class FileGrouper(object):
                         self.add_file(curr_group, os.path.join(root, file))
 
     def outliers(self):
+        '''
+        :return: Returns a list of the file groups that dont have the correct number of files (too many or too few)
+        Correct number of files determined by the number of files in the majority of the groups (mode group size)
+        '''
         return [x for x in self.groups.values() if x.outliers_extra or not self.group_full(x)]
 
     def group_full(self, group):
+        '''
+        :param group: Takes filegroup object as a parameter
+        :return: Boolean function True if the group from the parameter is full (correct number of files) and False if
+        the group is not full (too few files based on the desired number).
+        '''
         found_empty = False
         group_vars = vars(group)
         for x in self.type_map.keys():
@@ -55,6 +74,11 @@ class FileGrouper(object):
         return True
 
     def add_file(self, group, file):
+        '''
+        :param group: takes a file group object as one parameter (for files to be added to)
+        :param file: takes specific file as second parameter
+        :return: Does not return a value; updates variables such as vars(group) and group.empty based on input file
+        '''
         for type, func in self.type_map.items():
             if func(file):
                 vars(group)[type] = file
@@ -63,6 +87,12 @@ class FileGrouper(object):
                 return
 
     def prefix_in_groups(self, prefix):
+        '''
+        :param prefix: the subjectNumber_Month that precedes the file names,
+        indicating which subject the file has information about (format: ##_##).
+        :return: True if the prefix already has a group started in self;
+        False if it is the first file being analyzed with that specific prefix.
+        '''
         for group in self.groups:
             if group.prefix == prefix:
                 return True
@@ -70,16 +100,26 @@ class FileGrouper(object):
 
 
 class FileGroup(object):
+    '''
+    Initialize the FileGroup class.  Classes based on prefixes for the files in question.
+    file_types list contains the types of files in question (from filetypes.py module)
+    '''
     def __init__(self, file_types=[], prefix=""):
         for x in file_types:
             setattr(self, x, "")
 
+        #Initialize variables (prefix; full; empty; outlier_extra) to their desired values.
         self.prefix = prefix
         self.full = False
         self.empty = True
         self.outliers_extra = []
 
     def prefix_match(self, input):
+        '''
+        :param input: file to compare against self
+        :return: True if the input file has the same prefix as self, and is therefore of the same FileGroup;
+                 Returns false otherwise.
+        '''
         if input[:len(self.prefix)] == self.prefix:
             return True
         return False
--- filegrouper/filetypes.py
+++ filegrouper/filetypes.py
@@ -1,84 +1,146 @@
 class FileType(object):
+    '''
+    Initialize FileType class for 14 different types of files;
+    Incoorporates functions below when used in filegrouper.py
+    Takes 2 parameters: 1st - string name to call files of the desired type
+                        2nd - the type of file to look for (one of the comp_functions below)
+    '''
     def __init__(self, typename, comp_func):
         self.typename = typename
         self.comp_func = comp_func
 
 def audio_bl_comp_func(input):
+    '''
+    :param input: Input is a filename to be analyzed
+    :return: True if the file is a basic level audio file; false otherwise
+    '''
     if input.endswith(".csv"):
         if "audio" in input:
             return True
     return False
 
 def video_bl_comp_func(input):
+    '''
+    :param input: Input is a filename to be analyzed
+    :return: True if file is basic level video file; false otherwise
+    '''
     if input.endswith(".csv"):
         if "video" in input:
             return True
     return False
 
 def lena5min_comp_func(input):
+    '''
+    :param input: Input is a filename to be analyzed
+    :return: True if file is a lena5min.csv file; false otherwise
+    '''
     if input.endswith("lena5min.csv"):
         return True
     return False
 
 def silences_comp_func(input):
+    '''
+    :param input: Input is a filename to be analyzed
+    :return: True if file is silence.txt file; false otherwise
+    '''
     if input.endswith("silences.txt"):
         return True
     return False
 
 def video_comp_func(input):
+    '''
+    :param input: Input is a filename to be analyzed
+    :return: True if file is "video".mp4 file; false otherwise
+    '''
     if input.endswith(".mp4"):
         return True
     return False
 
 def audio_comp_func(input):
+    '''
+    :param input: Input is a filename to be analyzed
+    :return: True if "audio".wav file and not scrubbed; false otherwise
+    '''
     if input.endswith(".wav"):
         if "scrubbed" not in input:
             return True
     return False
 
 def audio_scrubbed_comp_func(input):
+    '''
+    :param input: Input is a filename to be analyzed
+    :return: True if "audio".wav file and is scrubbed; false otherwise
+    '''
     if input.endswith(".wav"):
         if "scrubbed" in input:
             return True
     return False
 
 def opf_not_final_comp_func(input):
+    '''
+    :param input: Input is a filename to be analyzed
+    :return: True if .opf file and is not final; false otherwise
+    '''
     if input.endswith(".opf"):
         if "consensus_final" not in input:
             return True
     return False
 
 def opf_final_comp_func(input):
+    '''
+    :param input: Input is a filename to be analyzed
+    :return: True if .opf file and is final; false otherwise
+    '''
     if input.endswith(".opf"):
         if "consensus_final" in input:
             return True
     return False
 
 def clan_silences_comp_func(input):
+    '''
+    :param input: Input is a filename to be analyzed
+    :return: True if .cha file with "silence"; false otherwise
+    '''
     if input.endswith(".cha"):
         if "silences" in input:
             return True
     return False
 
 def clan_final_comp_func(input):
+    '''
+    :param input: Input is a filename to be analyzed
+    :return: True if .cha file final but not merged (final clan); false otherwise
+    '''
     if input.endswith(".cha"):
         if "_final" in input and "_merged" not in input:
             return True
     return False
 
 def clan_merged_comp_func(input):
+    '''
+    :param input: Input is a filename to be analyzed
+    :return: True if .cha file merged but not final (merged clan); false otherwise
+    '''
     if input.endswith(".cha"):
         if "newclan_merged" in input and "final" not in input:
             return True
     return False
 
 def clan_merged_final_comp_func(input):
+    '''
+    :param input: Input is a filename to be analyzed
+    :return: True if .cha file, merged and final (final merged clan); false otherwise
+    '''
     if input.endswith(".cha"):
         if "newclan_merged" in input and "final" in input:
             return True
     return False
 
 def video_personal_info_comp_func(input):
+    '''
+    :param input: Input is a filename to be analyzed
+    :return: True if personal_info.csv video file; false otherwise
+    '''
     if input.endswith("personal_info.csv"):
         return True
     return False
